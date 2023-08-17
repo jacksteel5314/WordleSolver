8fd79ca39123f95cc5d3f322e802aeb50831ca83
jacksteel <jacksteel531@icloud.com>
2023-08-16 22:55:46 -0400

    Added new mechanism

--- suggestion-method.py
+++ suggestion-method.py
@@ -10,27 +10,54 @@ with open('wordle-La.txt') as f:
         word_bank[i] = word_bank[i].replace("\n", "")
         i+=1
 
+word_bank = np.array(word_bank);
+
 
 def suggestions(colors, letters):
     clean_colors = colors.lower().strip(" ")
     clean_letters = letters.lower().strip(" ")
     _valid_input(clean_colors, clean_letters)
-    
-def _valid_input(colors, letters): 
-    if colors.alpha(): 
 
-def _remove(colors, letters):
+def first_guess():
+    # create a word out of most common words in wordle dictionary
+    # most common letters / best positioning (do both)
+    first_word_suggestions = []
     for i in range(5):
-        if colors[i] == 'b':
-            word_bank.remove(word_bank.contains(letters[i]))
+        letter_counts = {}
+        for input in word_bank: 
+            letter = input[i]
+            letter_counts[letter] = letter_counts.get(letter, 0) + 1
+    sorted_letters = sorted(letter_counts, key= lambda x: letter_counts[x], reverse=True)
+    top_five = sorted_letters[:5]
+    list = {}
+    value = 0
+    for word in word_bank:
+        if sorted_letters[0] in word:
+            value += 100
+        if sorted_letters[1] in word:
+            value += 90
+        if sorted_letters[2] in word:
+            value += 80
+        if sorted_letters[3] in word:
+            value += 70
+        if sorted_letters[4] in word:
+            value += 60
+        if sorted_letters[5] in word:
+            value += 50
+        if sorted_letters[6] in word:
+            value += 40
+        if sorted_letters[7] in word:
+            value += 30
+        if sorted_letters[8] in word:
+            value += 20
+        if sorted_letters[9] in word:
+            value += 10
+        list[word] = value
+        value=0
+    sorted_words = sorted(list, key=lambda y: list[y], reverse=True)
+    return [sorted_words[1], sorted_words[1:10]]
 
+lists = first_guess()
+print("Your best first guess is:", lists[0])
+print("Here are eight other reasonable guesses: ", lists[1])
 
-def _most_common(word):
-    most_common = word_bank
-    i = 5
-    list = []
-    if(len(most_common) < i):
-        i = len(most_common)
-    for i in range(i):
-        list.append(most_common[random.randInt(0, len(most_common))])
-    return list
