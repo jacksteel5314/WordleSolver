291fba31e8601af9e4b68344f8cec974442ccb68
Jack Steel <jas333@duke.edu>
2023-09-07 14:39:48 -0400

    Pylint

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import suggestion 
+'''
+Main Method File
+'''
+import suggestion
 
-# Print
 def main():
+    '''
+    Main Method
+    '''
     input("Hello, welcome to Wordle Solver. To start, press enter.")
-    list = suggestion.best_guess()
-    print("The best possible choice for a first word is: " + list[0])
-    print("Other possible options in descending order are: " + list[1] + ", " + list[2] + ", " + list[3] + ", " + list[4] + ", " + list[5] + ", " + list[6] + ", " + list[7] + ", " + list[8] + ", and " + list[9])
+    list1 = suggestion.best_guess()
+    print("The best possible choice for a first word is: " + list1[0])
+    strg = "Other possible options in descending order are: "
+    for i in range(1, 10):
+        strg += (list1[i] + ", ")
+    print(strg[0:len(strg)-2])
     for i in range(1, 6):
         if len(suggestion.word_bank) == 1:
             print(f"Congratulations, you found your word: {suggestion.word_bank[0]}")
@@ -13,7 +21,7 @@ def main():
         while True:
             word = input(f"Input guess #{i}: ")
             colors = input(f"Input colors #{i}(B = black, Y = yellow, G = green): ")
-            try: 
+            try:
                 valid_input = suggestion.valid_input(colors, word)
             except ValueError as err:
                 print("Your Entry was Invalid. Try Again")
@@ -25,13 +33,12 @@ def main():
         except ValueError as err:
             print(err)
             break
-        list = suggestion.best_guess()
+        list1 = suggestion.best_guess()
         if len(list) == 1:
             continue
-        print(f"The best possible choice for word #{i+1} is: " + list[0])
-        print("Other possible options in descending order are: ", list[1:10])
+        print(f"The best possible choice for word #{i+1} is: " + list1[0])
+        print("Other possible options in descending order are: ", list1[1:10])
 
 
 if __name__ == '__main__':
     main()
-
